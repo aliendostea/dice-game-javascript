66862eac2a0d96eebd78441a935ef9897b3aa13c
angel liendo <aliendostea@gmail.com>
2020-08-25 10:05:20 +0200

    first commit dice game javascript

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dice game - JavaScript ⚡️
+
+## What I learn in this course 📋
+
+⚡️ The DOM and DOM manipulation.
+⚡️ Event handling.
+⚡️ DRY Principle.
+⚡️ How to use algorithms to solve problems.
+
+---
+
+
+## When I developed this course 
+
+I developed this project on 2018.
+
+## Author of the course 👥
+
+- **Jonas Schmedtmann** - [https://github.com/jonasschmedtmann](https://github.com/jonasschmedtmann)
+
---
+++ app.js
@@ -0,0 +1,109 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+let scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0; 
+
+
+// ---------------** getters **---------------
+let testGetter = document.querySelector('#current-0').textContent;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+// ---------------** change CSS **---------------
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function (){
+
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. display de result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update the round score if the rolled number was NOT a 1
+    if (dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // add current score to global score
+    scores[activePlayer] += roundScore; 
+
+    // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // check if player won the game
+                // scores[activePlayer] >= 20 ? 
+                // document.querySelector('#name-' + activePlayer).textContent = 'You win'
+                // : 
+                // nextPlayer();
+    if (scores[activePlayer] >= 50 ){
+        document.querySelector('#name-' + activePlayer).textContent = 'You win';
+        document.querySelector('.dice').style.sdisplay = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    } else{
+        nextPlayer();
+    }
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    //next player
+    nextPlayer();
+});
+
+function nextPlayer(){
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    // if(activePlayer === 0) {
+    //     activePlayer = 1;
+    // } else {
+    //     activePlayer = 0;
+    // }
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+    //diceDOM.style.display = 'none';
+}
+
+
+
+
+// ---------------** setters **---------------
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
